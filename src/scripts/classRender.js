64bc24b89ff821c19a9588b41166fcd2003119b9
Svetlana994 <07svetlana94@gmail.com>
2021-06-09 01:24:41 +0300

    modal

--- src/scripts/classRender.js
+++ src/scripts/classRender.js
@@ -2,6 +2,7 @@ import modalTemp from '../templates/modal.hbs'
 import galleryListTemp from '../templates/gallery.hbs';
 import testObject from './testObject.js';
 import GenresNames from './classFetchFilms.js';
+import FetchFilms from './classFetchFilms.js';
 
 
 class RenderData {
@@ -62,15 +63,73 @@ const RenderGallery = new RenderData(galleryListTemp, '.js-gallery-container');
 const genresNames = GenresNames.getGenres();
 RenderGallery.render(testObject, genresNames);
 
+// const renderModal = new RenderData(modalTemp, '.modal-wrapper');
+// renderModal.render(testObject);
 
 // хендлер висит на каждой item. Data - массив объектов, всех фильмов на страничке.
-const onItemClick = (event, data) => {
-  console.log(event.currentTarget.dataset.id); //каждая item уникальна и имеет свой айдишник!
-  console.log(data); // Тут массив всех объектов - нужный ищи по dataset
+const onItemClick = async (event, data) => {
+  //console.log(event.currentTarget.dataset.id); //каждая item уникальна и имеет свой айдишник!
+  //console.log(data); // Тут массив всех объектов - нужный ищи по dataset
   //open modal
+  //await getFilm(event.currentTarget.dataset.id);
+
+ //openModal();
+  
+  openModal(event.currentTarget.dataset.id)
+
+}
+
+async function getFilm(id) {
+    const a = await FetchFilms.getFilmById(id);
+    console.log(a);
+   const markup =  modalTemp(a);
+
+    const modalWrapper = document.querySelector('.modal-wrapper');
+    modalWrapper.insertAdjacentHTML('beforeend', markup)
+}
+
+async function openModal(filmId) {
+await getFilm(filmId)
+
+  const backdrop = document.querySelector('.backdrop');
+
+  backdrop.classList.remove('is-hidden');
+
+    window.addEventListener('keyup', onEscPress);
+    backdrop.addEventListener('click', onBackdropClick);
+
+   onBtnClose();
+  
+  function onBtnClose() {
+    const btnClose = document.querySelector('.modal-btn-close');
+
+  btnClose.addEventListener('click', () => {
+    backdrop.classList.add('is-hidden');
+  })
+}
+  
+  function onEscPress(e) {
+if (e.code === "Escape") {
+  onCloseModal();
+  }
+}
+
+  function onCloseModal() {
+  backdrop.classList.add('is-hidden');
+
+  window.removeEventListener("keyup", onEscPress);
+  backdrop.removeEventListener("click", onBackdropClick);
+}
+  
+  function onBackdropClick(e) {
+ if (e.target === e.currentTarget) {
+    onCloseModal();
+  }
+}
 }
 
 
 
+//export default RenderData
 
 
