c5d9dc360027313d3c7f963e4899d5ad3973ca47
Serhii Tomashkevich <19tomat@gmail.com>
2021-06-08 20:55:01 +0300

    ~update:class render

--- src/scripts/classFetchFilms.js
+++ src/scripts/classFetchFilms.js
@@ -1,18 +1,17 @@
 class FilmsAPI {
   constructor(key) {
     this.key = key;
-    this.getGenres();
   }
 
   async getGenres() {
     try {
-      const response = await fetch(`https://api.themoviedb.org/3/genre/list?api_key=${this.key}`)
+      const response = await fetch(`https://api.themoviedb.org/3/genre/list?api_key=${this.key}`);
       const genres = await response.json();
-      this.genres = genres.genres;
+      return genres.genres;
     } catch (error) {
       console.log(error);
     }
-  }  
+  }
 }
 
-export default new FilmsAPI('28349e2a7add56dc6493ba9645697aa9')
+export default new FilmsAPI('28349e2a7add56dc6493ba9645697aa9');
