da7864ae415c6cd6a5348b5c667095757dceab3f
RBord <rostyslavbord@gmail.com>
2021-06-08 17:44:53 +0300

    Update team-cards with adaptive

--- src/team-card.js
+++ src/team-card.js
@@ -8,13 +8,27 @@ for (let i = 0; i < cards.length; i++){
 function toRotate(evt) {
     const cardFront = this.querySelector('.front')
     const cardBack = this.querySelector('.back')
+    const undercard = this.querySelector('.under-card')
+    
+    console.log(evt.target.classList)
 
-    if (evt.target.classList.contains('front') || evt.target.tagName === 'IMG') {
+    if (evt.target.classList.contains('cat-img')) {
         cardFront.style.transform = 'perspective(600px) rotateY(-180deg)'
         cardBack.style.transform = 'perspective(600px) rotateY(0deg)'
-    } else if (evt.target.classList.contains('back') || evt.target.tagName === 'IMG') {
+
+        undercard.style.opacity = '1'
+        undercard.style.top = '105%'
+        undercard.style.transition = 'all 0.2s linear'
+        this.style.marginBottom = '100px'
+        
+    } else if (evt.target.classList.contains('team-photo')) {
         cardFront.style.transform = 'perspective(600px) rotateY(0deg)'
         cardBack.style.transform = 'perspective(600px) rotateY(180deg)'
+
+        undercard.style.opacity = '0'
+        undercard.style.top = '80%'
+        undercard.style.transition = 'all 0.2s linear'
+        this.style.marginBottom = '0px'
     }
     return;
 }
