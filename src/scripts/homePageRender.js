410a9d6df38985c1a8b2161f6b809cf456a267c1
Julia Kutsyn <yuliyaflm@gmail.com>
2021-06-14 19:20:46 +0300

    changes bgimages and other

---
+++ src/scripts/homePageRender.js
@@ -0,0 +1,24 @@
+// import FilmAPI from './classFetchFilms';
+// import notifications from './notifications';
+
+// const movieAPI = new FilmAPI;
+
+// const refs = {
+//   galleryRef: document.querySelector('.gallery-list'),
+// }
+
+// getTrendingFilm();
+
+// function fetchMoviesByQuery(event) {
+//   event.preventDefault();
+//   movieAPI.query = event.target.elements.search.value;
+
+//   // movieAPI.getGenres().then
+
+//   clearGallery();
+
+// }
+
+// function clearGallery() {
+//   refs.galleryRef.innerHTML = '';
+// }
