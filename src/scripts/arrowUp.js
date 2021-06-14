6390c7bbfa2a949cf08a96afab2d1af58423c860
Alina Kotsiubynska <alinakotsiubynska@gmail.com>
2021-06-15 00:31:07 +0300

    adds arrow-up markup and styles

---
+++ src/scripts/arrowUp.js
@@ -0,0 +1,19 @@
+import arrowTmp from './tamplates/arrowUp.hbs';
+const utillsRoot = document.getElementById('utills')
+const Observer = new IntersectionObserver()
+
+const arrowHTML = arrowTmp();
+utillsRoot.insertAdjacentElement('beforeend', arrowHTML)
+const upBtn = document.getElementById('arrow-up')
+
+upBtn.addEventListener('click', )
+
+
+function scrollCheck() {
+  
+}
+function scrollToTop() {
+  
+}
+
+
